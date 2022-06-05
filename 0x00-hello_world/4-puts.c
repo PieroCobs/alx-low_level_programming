913207de795ae0600b9840879a055fa742cab155
Peter Sabla <pcobby@ymail.com>
2022-06-05 02:04:33 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - starting point of program
+ * @parameter1: void - accepts no parameters
+ * Description: prints a string to the standard output
+ * Return: integer 0
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
