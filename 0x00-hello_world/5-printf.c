cf5af00fe97b0ecab3d763659fb5ed2d091ebd6d
Peter Sabla <pcobby@ymail.com>
2022-06-05 02:23:00 -0700

    Helo, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of program
+ * Description: prints an formated string to standard output
+ * Return: integer 0
+ */
+int main(void)
+{
+	char comma = ",";
+
+	printf("with proper grammer%c but the outcome is a piece of art%c\n", comma);
+	return (0);
+}
