4dd50092e5a0772fc923b8384ca2131b667f365f
Peter Sabla <pcobby@ymail.com>
2022-06-07 06:55:13 -0700

    Print base 10 single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - pogram starting point
+* Description: prints all single digit base 10 numbers
+* Return: integer 0
+*/
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
