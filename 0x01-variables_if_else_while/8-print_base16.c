4018592f8abf40bc66a5825a4d13e2fd0a327794
Peter Sabla <pcobby@ymail.com>
2022-06-07 08:46:04 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int step = 0;
-	char hexDigits[17] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'};
+	char hexDigits[17] = "0123456789abcdef";
 
 	while (step < 16)
 	{
