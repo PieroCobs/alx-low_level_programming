fbfef937443044fa3dc90eceb4d78a60537e4a08
Peter Sabla <pcobby@ymail.com>
2022-06-16 09:31:57 -0700

    Prints every minute of the day of Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* jack_bauer - print every minute of the day
+* of Jack Bauer, starting from 00:00 to 23:00
+*
+* Return: void
+*/
+void jack_bauer(void)
+{
+	int i = 0, j;
+
+	while (i < 24)
+	{
+		if (i < 10)
+		{
+			_putchar('0');
+		}
+
+		_putchar(i);
+		_Putchar(':');
+
+		j = 0;
+
+		while (j < 60)
+		{
+			if (j < 10)
+			{
+				_putchar('0');
+			}
+
+			_putchar(j);
+			_putchar('\n');
+
+			j++;
+		}
+
+		i++;
+	}
+}
