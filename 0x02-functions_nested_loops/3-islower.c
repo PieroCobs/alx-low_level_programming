7c25678fc71813ceb1ba396a1d3a0dccd46c5278
Peter Sabla <pcobby@ymail.com>
2022-06-16 04:11:50 -0700

    Correction: checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,13 @@
 
 /**
 * _islower - checks for lowercase letter
+* @c: the character to check
+*
 * Return: 1 if check is true, 0 if false
 */
 int _islower(int c)
 {
-	if (c >= 'a' && ch <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
