0216ee3b22106d5822f98703ed14017e2b13555f
Peter Sabla <pcobby@ymail.com>
2022-06-16 03:51:18 -0700

    Correction: prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,12 @@
 void print_alphabet_x10(void)
 {
 	char alpha[] = "abcdefghijklmnopqrstuvwxyz\n";
-	int alpha_count = 0, times_count = 0;
+	int times_count = 0, alpha_count;
 
 	while (times_count < 10)
 	{
+		alpha_count = 0;
+
 		while (alpha_count < 27)
 		{
 			_putchar(alpha[alpha_count]);
@@ -18,5 +20,6 @@ void print_alphabet_x10(void)
 		}
 
 		times_count++;
+
 	}
 }
