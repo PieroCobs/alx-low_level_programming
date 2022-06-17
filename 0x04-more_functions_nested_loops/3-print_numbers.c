53e8a4ebd57df5ec90703aa9ccc2f491b9d44247
Peter Sabla <pcobby@ymail.com>
2022-06-17 08:15:17 -0700

    Correction: 3. Prints numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,16 @@
 /**
 * print_numbers - prints numbers from 0 to 9,
 * followed by a new line
-*
 * Return: void
 */
 void print_numbers(void)
 {
+	char nums[] = "0123456789\n";
 	int i = 0;
 
-	while (i <= 9)
+	while (i <= 11)
 	{
-		_putchar(i);
+		_putchar(nums[i]);
 		i++;
 	}
 }
